56e7aa6a0436e4220f2f7639a288b46e29fa0ab0
BrunoSette <brunosette@gmail.com>
2015-01-10 03:22:01 -0300

    Ajuste no endereço do banco de dados

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 	
 	// configurações do banco de dados
 
-	define("DB_HOST", "wordpress.ccnwocwomc7r.sa-east-1.rds.amazonaws.com:3307");
+	define("DB_HOST", "wordpress.ccnwocwomc7r.sa-east-1.rds.amazonaws.com");
 	define("DB_NAME", "db_estudandoonline");
 	define("DB_USER", "brunosette");
 	define("DB_PASSWORD", "voupassar7777");
