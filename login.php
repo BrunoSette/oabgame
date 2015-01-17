28ccc7768833b6808f2530f67e48e148ea9ca102
BrunoSette <brunosette@gmail.com>
2015-01-17 04:00:32 +0000

    Add Nova Logo e Favicon

--- login.php
+++ login.php
@@ -1,9 +1,26 @@
 <!doctype html>
 <html lang="pt">
 <head>
+	<link rel="apple-touch-icon" sizes="57x57" href="/favicons/apple-touch-icon-57x57.png">
+	<link rel="apple-touch-icon" sizes="114x114" href="/favicons/apple-touch-icon-114x114.png">
+	<link rel="apple-touch-icon" sizes="72x72" href="/favicons/apple-touch-icon-72x72.png">
+	<link rel="apple-touch-icon" sizes="144x144" href="/favicons/apple-touch-icon-144x144.png">
+	<link rel="apple-touch-icon" sizes="60x60" href="/favicons/apple-touch-icon-60x60.png">
+	<link rel="apple-touch-icon" sizes="120x120" href="/favicons/apple-touch-icon-120x120.png">
+	<link rel="apple-touch-icon" sizes="76x76" href="/favicons/apple-touch-icon-76x76.png">
+	<link rel="apple-touch-icon" sizes="152x152" href="/favicons/apple-touch-icon-152x152.png">
+	<link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon-180x180.png">
+	<link rel="icon" type="image/png" href="/favicons/favicon-192x192.png" sizes="192x192">
+	<link rel="icon" type="image/png" href="/favicons/favicon-160x160.png" sizes="160x160">
+	<link rel="icon" type="image/png" href="/favicons/favicon-96x96.png" sizes="96x96">
+	<link rel="icon" type="image/png" href="/favicons/favicon-16x16.png" sizes="16x16">
+	<link rel="icon" type="image/png" href="/favicons/favicon-32x32.png" sizes="32x32">
+	<meta name="msapplication-TileColor" content="#00aba9">
+	<meta name="msapplication-TileImage" content="/favicons/mstile-144x144.png">
+
 	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2.0" />
-	<title>Provas da OAB Game</title>
+	<title>AprovaGame</title>
 	<link rel="stylesheet" href="css/all.css" />
 	<link rel="stylesheet" href="http://cdn.aprovagame.com.br/css/glycon_boots.css" />
 	<link rel="stylesheet" href="http://cdn.aprovagame.com.br/css/bootstrap.min.css" />
@@ -39,7 +56,7 @@
 			<div class="row">
 			<div class="col-md-4">
 				<figure>
-					<img src="http://cdn.aprovagame.com.br/img/logo.png" alt="Logomarca AprovaGame" />
+					<img src="img/logo.png" alt="Logomarca AprovaGame" />
 				</figure>
 			</div>
 			<div class="col-md-6">
