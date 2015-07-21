1c2a3af443feed9dba3544928cec0f6c30960894
Bruno Sette <brunosette@gmail.com>
2015-07-21 01:33:50 -0300

    Atualizar Banco Digital Ocean

--- game/api/config.php
+++ game/api/config.php
@@ -2,9 +2,9 @@
 	
 	// configurações do banco de dados
 
-	define("DB_HOST", "wordpress.ccnwocwomc7r.sa-east-1.rds.amazonaws.com");
-	define("DB_NAME", "db_estudandoonline");
-	define("DB_USER", "brunosette");
+	define("DB_HOST", "localhost");
+	define("DB_NAME", "aprovagame");
+	define("DB_USER", "root");
 	define("DB_PASSWORD", "voupassar7777");
 
 	// Lista de Erros
@@ -23,8 +23,4 @@
 	define("INICIADA", 4);
 	define("ENCERRADA", 5);
 
-// define("DB_HOST", "localhost");
-// 	define("DB_NAME", "db_estudandoonline");
-// 	define("DB_USER", "root");
-// 	define("DB_PASSWORD", "root");
 ?>
