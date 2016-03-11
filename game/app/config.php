13499bc6b437af2fac78d370721738fa3d9f261b
BrunoSette <brunosette@gmail.com>
2016-03-11 09:35:16 -0300

    Update Database Password

--- game/app/config.php
+++ game/app/config.php
@@ -5,10 +5,6 @@
 	define("DB_HOST", "localhost");
 	define("DB_NAME", "aprovagame");
 	define("DB_USER", "root");
-	define("DB_PASSWORD", "voupassar7777");
+	define("DB_PASSWORD", "VouPassar7777!@#$");
 
-// define("DB_HOST", "localhost");
-// 	define("DB_NAME", "db_estudandoonline");
-// 	define("DB_USER", "root");
-// 	define("DB_PASSWORD", "root");
 ?>
