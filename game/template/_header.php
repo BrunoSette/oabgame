e807410e3851e6ba19c9088ce7fc94abe3e8ebd6
Marcel Santana <mss8@cin.ufpe.br>
2019-03-08 20:48:25 -0300

    Nova pagina de login.

--- game/template/_header.php
+++ game/template/_header.php
@@ -34,15 +34,6 @@
 </head>
 <body>
 	<div id="fb-root"></div>
-	<script>
-		(function(d, s, id) {
-		  var js, fjs = d.getElementsByTagName(s)[0];
-		  if (d.getElementById(id)) return;
-		  js = d.createElement(s); js.id = id;
-		  js.src = "//connect.facebook.net/pt_BR/sdk.js#xfbml=1&appId=604815266237503&version=v2.0";
-		  fjs.parentNode.insertBefore(js, fjs);
-		}(document, 'script', 'facebook-jssdk'));
-	</script>
 	
 	<header class="topo">	
 	<nav class="navbar navbar-inverse">
@@ -103,7 +94,4 @@
       </div><!-- container -->
     </nav> 
 		
-	</header>
-
-
-	
+	</header>	
