b3c5b8cfb6743a694983a64a4a50d4813dd817a6
Marcel Santana <mss8@cin.ufpe.br>
2019-03-09 15:47:49 -0300

    Comecei a melhorar a pagina de perfil.

--- game/js/perfil.js
+++ game/js/perfil.js
@@ -2,81 +2,94 @@ function getBadges() {
     var pins = [];
     pins[badgesEnum.TRES_ACERTOS_SEGUIDOS] =    {   "src" : clienteUrl + "/img/badges/seguidos3-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte três questões seguidas e ganhe."
+                                                    "ganhar" : "Acerte três questões seguidas e ganhe.",
+                                                    "ganhou" : "Você acertou três questões seguidas."
                                                 };
 
     pins[badgesEnum.SETE_ACERTOS_SEGUIDOS] =    {  "src" : clienteUrl + "/img/badges/seguidos7-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte sete questões seguidas e ganhe."
+                                                    "ganhar" : "Acerte sete questões seguidas e ganhe.",
+                                                    "ganhou" : "Você acertou sete questões seguidas."
                                                 };
     
     pins[badgesEnum.DEZ_ACERTOS_SEGUIDOS] =     {   "src" : clienteUrl + "/img/badges/seguidos10-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte dez questões seguidas e ganhe."
+                                                    "ganhar" : "Acerte dez questões seguidas e ganhe.",
+                                                    "ganhou" : "Você acertou dez questões seguidas."
                                                 };
     
     pins[badgesEnum.QUINZE_ACERTOS_SEGUIDOS] =  {
                                                     "src" : clienteUrl + "/img/badges/seguidos15-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte quinze questões seguidas e ganhe."
+                                                    "ganhar" : "Acerte quinze questões seguidas e ganhe.",
+                                                    "ganhou" : "Você acertou quinze questões seguidas."
                                                 };
     
     pins[badgesEnum.DEZ_RESPOSTAS_CORRETAS] =   {
                                                     "src" : clienteUrl + "/img/badges/acertou10-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte dez questões e ganhe."
+                                                    "ganhar" : "Acerte dez questões e ganhe.",
+                                                    "ganhou" : "Você acertou dez questões.",
                                                 };
     
     
     pins[badgesEnum.VINTE_RESPOSTAS_CORRETAS] = {
                                                     "src" : clienteUrl + "/img/badges/acertou20-180.png", 
                                                     "exibe" : false,
-                                                    "desc" : "Acerte vinte questões e ganhe."
+                                                    "ganhar" : "Acerte vinte questões e ganhe.",
+                                                    "ganhou" : "Você acertou vinte questões.",
                                                 };
     
     
     pins[badgesEnum.CINQUENTA_RESPOSTAS_CORRETAS] = {
                                                         "src" : clienteUrl + "/img/badges/acertou50-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Acerte cinquenta questões e ganhe."
+                                                        "ganhar" : "Acerte cinquenta questões e ganhe.",
+                                                        "ganhou" : "Você acertou cinquenta questões."
                                                     };
     
 
     pins[badgesEnum.CEM_RESPOSTAS_CORRETAS] =       {
                                                         "src" : clienteUrl + "/img/badges/acertou100-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Acerte cem questões e ganhe."
+                                                        "ganhar" : "Acerte cem questões e ganhe.",
+                                                        "ganhou" : "Você acertou cem questões."
                                                     };
     
 
     pins[badgesEnum.DUZENTAS_RESPOSTAS_CORRETAS] =  {
                                                         "src" : clienteUrl + "/img/badges/acertou200-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Acerte duzentas questões e ganhe."
+                                                        "ganhar" : "Acerte duzentas questões e ganhe.",
+                                                        "ganhou" : "Você acertou duzentas questões."
                                                     };
     
     pins[badgesEnum.JOGOU_2X] =                     {
                                                         "src" : clienteUrl + "/img/badges/jogou2x-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Jogue duas vezes e ganhe."
+                                                        "ganhar" : "Jogue duas vezes e ganhe.",
+                                                        "ganhou" : "Você jogou duas vezes."
                                                     }; 
     
     pins[badgesEnum.JOGOU_5X] =                     {
                                                         "src" : clienteUrl + "/img/badges/jogou5x-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Jogue cinco vezes e ganhe."
+                                                        "ganhar" : "Jogue cinco vezes e ganhe.",
+                                                        "ganhou" : "Você jogou cinco vezes."
                                                     };
     
     pins[badgesEnum.JOGOU_10X] =                    {
                                                         "src" : clienteUrl + "/img/badges/jogou10x-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Jogue dez vezes e ganhe."
+                                                        "ganhar" : "Jogue dez vezes e ganhe.",
+                                                        "ganhou" : "Você jogou dez vezes."
                                                     };
     
     pins[badgesEnum.JOGOU_15X] =                    {
                                                         "src" : clienteUrl + "/img/badges/jogou15x-180.png", 
                                                         "exibe" : false,
-                                                        "desc" : "Jogue quinze vezes e ganhe."
+                                                        "ganhar" : "Jogue quinze vezes e ganhe.",
+                                                        "ganhou" : "Você jogou quinze vezes."
                                                     };
 
     return pins;
@@ -99,6 +112,7 @@ function userProfile(profile) {
     var img_url = GeneroNivelImageURL[genero][profile.nivel-1];
     var userImage = profile.larger_foto_profile ? profile.larger_foto_profile : img_url;
 
+    // Cria HTML do perfil.
     var html = "<div class='mb10' style='overflow: hidden'>";
         html +=     "<img class='left mr10' src='"+userImage+"' alt='' />";
         html +=     "<p class='large'>" + profile.nome + "</p>";
@@ -111,12 +125,15 @@ function userProfile(profile) {
 
         html += "<ul>";
         for (i in pins) {
-            if (pins[i].exibe)
+            if (pins[i].exibe) {
                 html += "<li style='padding: 5px 15px;' class='mr10'><img class='left' src='"+pins[i].src+"' style='width:10%;'/>";
-            else
+                html +=     "<span style='line-height: 85px; margin-left: 20px'>"+pins[i].ganhou+"</span>";                
+            }
+            else {
                 html += "<li style='padding: 5px 15px;' class='mr10'><img class='left' src='" + pins[i].src + "' style='width:10%; opacity: 0.35'/>";
+                html +=     "<span style='line-height: 85px; margin-left: 20px'>"+pins[i].ganhar+"</span>";
+            }
 
-            html +=     "<span style='line-height: 85px'>"+pins[i].desc+"</span>";
             html += "</li>";
         }
         html += "</ul>";
