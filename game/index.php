8f406489ecb29cd6064c1f377bf164b6e472c06c
Marcel Santana <mss8@cin.ufpe.br>
2019-03-12 18:49:16 -0300

    Test

--- game/index.php
+++ game/index.php
@@ -1,6 +1,12 @@
 <?php
 session_start(); 
 
+if ($_SERVER['HTTPS'] != "on") {
+    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+    header("Location: $url");
+    exit;
+}
+
 if (isset($_SESSION['FBID'])){
 	if($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http'){
 		 header("Location: https://www.oabgame.com.br/game/#_=_");
