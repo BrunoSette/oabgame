dd2422c355ceed3aa6af24e0e7adfd94cfedf3e3
Marcel Santana <mss8@cin.ufpe.br>
2019-03-14 21:51:47 -0300

    Test

--- game/index.php
+++ game/index.php
@@ -1,11 +1,11 @@
 <?php
 session_start(); 
 
-if ($_SERVER['HTTPS'] != "on") {
-    $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
-    //header("Location: $url");
-    //exit;
-}
+// if ($_SERVER['HTTPS'] != "on") {
+//     $url = "https://". $_SERVER['SERVER_NAME'] . $_SERVER['REQUEST_URI'];
+//     //header("Location: $url");
+//     //exit;
+// }
 
 if (isset($_SESSION['FBID'])){
 	if($_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http'){
