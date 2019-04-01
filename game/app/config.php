5424606e1be20e83754fea482aef4a6fdf7f38f1
BrunoSette <brunosette@gmail.com>
2019-04-01 15:36:09 -0300

    change OABConfig to Localhost

--- game/app/config.php
+++ game/app/config.php
@@ -2,7 +2,7 @@
 	
 	// configurações do banco de dados
 
-	define("DB_HOST", "oabgame2");
+	define("DB_HOST", "localhost");
 	define("DB_NAME", "aprovagame");
 	define("DB_USER", "wordpress");
 	define("DB_PASSWORD", "VouPassar7777!@#$");
