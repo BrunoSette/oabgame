ee68fc03935b49c72a20b238f194148aa11d8641
brunomx <brunomx01@gmail.com>
2019-04-02 15:09:25 -0300

    Debug game/fbconfig.php

--- game/fbconfig.php
+++ game/fbconfig.php
@@ -18,17 +18,17 @@ $facebook = new \Facebook\Facebook([
 
 $helper = $facebook->getRedirectLoginHelper();
 
-try {
+// try {
     $accessToken = $helper->getAccessToken();
-} catch (Facebook\Exceptions\FacebookResponseException $e) {
-    // When Graph returns an error
-    echo 'Graph returned an error: ' . $e->getMessage();
-    exit;
-} catch (Facebook\Exceptions\FacebookSDKException $e) {
-    // When validation fails or other local issues
-    echo 'Facebook SDK returned an error: ' . $e->getMessage();
-    exit;
-}
+// } catch (Facebook\Exceptions\FacebookResponseException $e) {
+//     // When Graph returns an error
+//     echo 'Graph returned an error: ' . $e->getMessage();
+//     exit;
+// } catch (Facebook\Exceptions\FacebookSDKException $e) {
+//     // When validation fails or other local issues
+//     echo 'Facebook SDK returned an error: ' . $e->getMessage();
+//     exit;
+// }
 
 
 if (!isset($accessToken)) {
