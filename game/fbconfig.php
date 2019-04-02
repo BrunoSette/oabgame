12b366d9d87e8cdb57eb5ddbb3677687c60ffbf6
brunomx <brunomx01@gmail.com>
2019-04-02 12:52:05 -0300

    Debug fbconfig

--- game/fbconfig.php
+++ game/fbconfig.php
@@ -9,7 +9,7 @@ require 'app/vendor/autoload.php';
 
 $currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-$currentUrl = str_replace('fbconfig', 'fb-callback', $currentUrl);
+// $currentUrl = str_replace('fbconfig', 'fb-callback', $currentUrl);
 
 $facebook = new \Facebook\Facebook([
     'app_id' => '604815266237503',
