42fe71266e571aaa4c532fb358e95f18199e78df
brunomx <brunomx01@gmail.com>
2019-04-02 14:23:25 -0300

    Debug game/fbconfig.php

--- game/fbconfig.php
+++ game/fbconfig.php
@@ -9,10 +9,6 @@ require 'app/vendor/autoload.php';
 
 $currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
-$currentUrl = str_replace('fbconfig', 'fb-callback', $currentUrl);
-
-$currentUrl = "https: //www.oabgame.com.br/game/fb-callback";
-
 $facebook = new \Facebook\Facebook([
     'app_id' => '604815266237503',
     'app_secret' => 'bf44e169874255d0facd3f48d0cd2981',
@@ -21,9 +17,33 @@ $facebook = new \Facebook\Facebook([
 
 $helper = $facebook->getRedirectLoginHelper();
 
-$permissions = ['email', 'public_profile', 'user_location', 'user_birthday'];
+try {
+    $accessToken = $helper->getAccessToken();
+} catch (Facebook\Exceptions\FacebookResponseException $e) {
+    // When Graph returns an error
+    echo 'Graph returned an error: ' . $e->getMessage();
+    exit;
+} catch (Facebook\Exceptions\FacebookSDKException $e) {
+    // When validation fails or other local issues
+    echo 'Facebook SDK returned an error: ' . $e->getMessage();
+    exit;
+}
 
-$loginUrl = $helper->getLoginUrl($currentUrl, $permissions);
-// var_dump(htmlspecialchars($loginUrl));die();
-header("Location: " . $loginUrl);
+
+if (!isset($accessToken)) {
+  if ($helper->getError()) {
+      header('HTTP/1.0 401 Unauthorized');
+      echo "Error: " . $helper->getError() . "\n";
+      echo "Error Code: " . $helper->getErrorCode() . "\n";
+      echo "Error Reason: " . $helper->getErrorReason() . "\n";
+      echo "Error Description: " . $helper->getErrorDescription() . "\n";
+  } else {
+    $permissions = ['email', 'public_profile', 'user_location', 'user_birthday'];
+
+    $loginUrl = $helper->getLoginUrl($currentUrl, $permissions);
+    header("Location: " . $loginUrl);
+  }
+} else {
+  var_dump($accessToken);die();
+}
 ?>
