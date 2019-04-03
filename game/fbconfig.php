c567b120e720fbca80593a3f3edf27baa7309a6c
Bruno Sette <brunosette@gmail.com>
2019-04-03 00:05:44 +0000

    Update fbconfig.php

--- game/fbconfig.php
+++ game/fbconfig.php
@@ -8,7 +8,7 @@ require 'app/includes/utilities.php';
 require 'app/vendor/autoload.php';
 
 $currentUrl = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-$currentUrl = "https://www.oabgame.com.br/game";
+$currentUrl = "https://www.oabgame.com.br/game/fbconfig.php";
 
 $facebook = new \Facebook\Facebook([
     'app_id' => '604815266237503',
@@ -42,6 +42,7 @@ if (!isset($accessToken)) {
     $permissions = ['email', 'public_profile', 'user_location', 'user_birthday'];
 
     $loginUrl = $helper->getLoginUrl($currentUrl, $permissions);
+//      echo $loginUrl;
     header("Location: " . $loginUrl);
   }
 }
